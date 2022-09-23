04edb4f0bc39693612eaba789b5c921664d9f6a1
Saniya15 <Saniyafirdous1508@gmail.com>
2022-09-23 11:40:02 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare function func():any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Drum-Kit-Project';
+  ngOnInit():void
+  {
+    func();
+  }
 }
